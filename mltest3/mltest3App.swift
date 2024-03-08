f6f998e67e0aab53f7669a631f356678382231ca
vansh sharma <vb2810@srmist.edu.in>
2024-03-09 01:37:10 +0530

    Initial Commit

---
+++ mltest3/mltest3App.swift
@@ -0,0 +1,17 @@
+//
+//  mltest3App.swift
+//  mltest3
+//
+//  Created by SHHH!! private on 09/03/24.
+//
+
+import SwiftUI
+
+@main
+struct mltest3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
