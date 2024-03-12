a67bf263cf352b5a421eacf0efef436f88d6d8f7
vansh sharma <vb2810@srmist.edu.in>
2024-03-12 09:55:22 +0530

    first commit

---
+++ mltest3/ImagePicker.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePicker.swift
+//  mltest3
+//
+//  Created by SHHH!! private on 09/03/24.
+//
+
+import SwiftUI
+
+struct ImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePicker()
+}
---
+++ mltest3/UIImage+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIImage+Extension.swift
+//  mltest3
+//
+//  Created by SHHH!! private on 09/03/24.
+//
+
+import Foundation
